61ae026238549eae58ca9ca69a9d29815f843f0c
Mehar62 <78727941+Mehar62@users.noreply.github.com>
2023-10-07 16:54:20 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+function a(){
+    window.location = "narrow.html";
+}
+
+function back(){
+    window.location = "index.html";
+}
+img = document.getElementById('class');
+
+function aaa(){
+    img.classList.toggle('back')
+}
